d54d29b9811b7032b617aebf6f3024d05df04d5a
SirMateusz <93150693+SirMateusz@users.noreply.github.com>
2022-09-24 22:35:45 +0200

    Added weather API

--- config.js
+++ config.js
@@ -26,14 +26,14 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: 'e6d743328d29441b5c562c3ac0666f8b', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '52.229',
+	defaultLongitude: '21.012',
 
 	// Autochange
 	autoChangeTheme: true,
