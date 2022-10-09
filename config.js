1736e49dfbe135a378da8c935719f9725ed42d20
SirMateusz <93150693+SirMateusz@users.noreply.github.com>
2022-10-09 15:55:26 +0200

    Changed weatherIcons

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: 'e6d743328d29441b5c562c3ac0666f8b', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'White', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
